6139577453d57c9f5e2484b2da8dbd9be5604719
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-04 16:56:49 +0200

    Initialize SensorReadingAPI service and configure Ktor client
    This commit introduces an empty `SensorReadingAPI.kt` file within the `org.smartroots.data.service` package.
    
    It also updates `KtorClient.kt` to define a Koin module named `ktorClientModule`. This module provides named singletons for local and remote base URLs, both currently set to "http://192.168.8.14/". The `createHttpClient` function is retained for creating Ktor HttpClient instances.

--- composeApp/src/commonMain/kotlin/org/smartroots/data/module/KtorClient.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/module/KtorClient.kt
@@ -6,10 +6,12 @@ import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
 import org.smartroots.data.model.Sensor
 
 
-private const val BASE_URL = "http://192.168.8.14/"
+
 fun createHttpClient(baseURL: String): HttpClient{
    return HttpClient(){
         install(ContentNegotiation){
@@ -21,7 +23,10 @@ fun createHttpClient(baseURL: String): HttpClient{
 
     }
 }
-
+val ktorClientModule = module {
+ single(named("BASE_URL_LOCAL")){"http://192.168.8.14/"}
+ single(named("BASE_URL_REMOTE")){"http://192.168.8.14/"}
+}
 
 
 
