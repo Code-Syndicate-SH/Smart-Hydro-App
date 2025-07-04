7023fa2d88cea5d4808a5b0f9ece16366fa6e69a
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-04 18:00:53 +0200

    Implement Koin dependency injection for SensorReading
    This commit introduces dependency injection for `ApiSensorReadingImpl` using Koin.
    It defines a Koin module `SensorReadingModule` to provide an instance of `ApiSensorReadingImpl`, injecting `HttpClient`, `BASE_URL_LOCAL`, and `BASE_URL_REMOTE` dependencies.

--- composeApp/src/commonMain/kotlin/org/smartroots/data/service/apiSensorReadingImpl.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/service/apiSensorReadingImpl.kt
@@ -3,12 +3,14 @@ package org.smartroots.data.service
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import kotlinx.serialization.descriptors.PrimitiveKind
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
+import org.koin.core.component.getScopeName
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.smartroots.data.model.Sensor
-import org.smartroots.data.module.createHttpClient
+import org.koin.dsl.binds
 
 class ApiSensorReadingImpl(val tentClient: HttpClient,val localURL:String,val remoteUrl:String): ApiSensorReading{
     override suspend fun getSensorReading(isLocal: Boolean): Sensor {
@@ -19,4 +21,9 @@ class ApiSensorReadingImpl(val tentClient: HttpClient,val localURL:String,val re
         }
         return tentClient.get("$baseURL/r/n/r/n").body()
     }
+}
+val SensorReadingModule = module{
+ single<ApiSensorReading> {params->
+     ApiSensorReadingImpl(tentClient = get(), localURL = get(named("BASE_URL_LOCAL")), remoteUrl = get(named("BASE_URL_REMOTE")) )
+ }
 }
