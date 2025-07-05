e81d72a11803ff56da80ffe6e60809e51783d8b7
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-05 16:28:08 +0200

    Define SensorRepository interface and initial SensorRepositoryImpl
    This commit introduces the `SensorRepository` interface, which defines methods for interacting with sensor data and controls. It includes functions to:
    
    - Fetch current and historical sensor readings.
    - Toggle light, fan, extractor, and pump.
    - Control EC (Electrical Conductivity) levels (EC, EC Up, EC Down).
    - Control pH levels (pH, pH Up, pH Down).
    
    An empty `SensorReposityImpl` class is also added as a placeholder for the concrete implementation of this interface.

---
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/repository/SensorRepository.kt
@@ -0,0 +1,40 @@
+package org.smartroots.data.repository
+
+import io.ktor.client.statement.HttpResponse
+import org.smartroots.data.model.Sensor
+
+interface SensorRepository {
+    suspend fun fetchSensorReading(): Sensor
+    suspend fun fetchHistoricSensorReadings(): List<Sensor>
+
+    //light
+    suspend fun toggleLight(): HttpResponse
+
+    //fan
+    suspend fun toggleFan(): HttpResponse
+
+    //extract
+    suspend fun toggleExtractor(): HttpResponse
+
+    //pump
+    suspend fun togglePump(): HttpResponse
+
+    //ec
+    suspend fun ec(): HttpResponse
+
+    //ecUp
+    suspend fun ecUp(): HttpResponse
+
+    //ecDown
+    suspend fun ecDown(): HttpResponse
+
+    //ph
+    suspend fun pH(): HttpResponse
+
+    //phUp
+    suspend fun pHUp(): HttpResponse
+
+    //phDown
+    suspend fun pHDown(): HttpResponse
+
+}
---
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/repository/SensorReposityImpl.kt
@@ -0,0 +1,4 @@
+package org.smartroots.data.repository
+
+class SensorReposityImpl {
+}
