0ae5e65c48bb9ffa06fb0e1ea6fafbddfc76d4d9
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-06 21:17:20 +0200

    Fix typo and update NetworkConfigState
    This commit fixes a typo in the `NetworkConfigState` class name and updates its properties.
    
    - Renamed `NetoworkConfigState` to `NetworkConfigState`.
    - `currentConnectionStatus` in `NetworkConfigState` is now nullable and defaults to `NetworkConnection.UNKNOWN_CONNECTION_TYPE`.
    - `konnection` in `NetworkConfigState` now defaults to `Konnection.instance`.
    
    The `NetworkConfig` class has been updated to use the modified `NetworkConfigState`:
    - `_currentConnectionStatus` is now a `MutableStateFlow` of `NetworkConfigState`.
    - `currentConnectionState` is exposed as a `StateFlow` of `NetworkConfigState`.
    - The `checkConnectionStatus` function now updates the `_currentConnectionStatus` with the current network connection status from the `Konnection` instance and returns the current connection status.

---
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/model/NetworkConfigState.kt
@@ -0,0 +1,10 @@
+package org.smartroots.data.model
+
+import dev.tmapps.konnection.Konnection
+import dev.tmapps.konnection.NetworkConnection
+
+
+data class NetworkConfigState(
+    val currentConnectionStatus: NetworkConnection? = NetworkConnection.UNKNOWN_CONNECTION_TYPE,
+    val konnection: Konnection = Konnection.instance
+)
--- composeApp/src/commonMain/kotlin/org/smartroots/data/module/NetworkConfig.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/module/NetworkConfig.kt
@@ -1,21 +1,26 @@
 package org.smartroots.data.module
 
-import dev.tmapps.konnection.Konnection
 import dev.tmapps.konnection.NetworkConnection
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import org.koin.core.component.getScopeId
+import org.smartroots.data.model.NetworkConfigState
 
 class NetworkConfig() {
-    private val currentConnectionStatus =
-        MutableStateFlow<NetworkConnection>(NetworkConnection.UNKNOWN_CONNECTION_TYPE)
-    private val  konnection:Konnection = Konnection.instance
-    init {
-        checkConnectionStatus()
-    }
+    private val _currentConnectionStatus =
+        MutableStateFlow<NetworkConfigState>(NetworkConfigState())
+    val currentConnectionState: StateFlow<NetworkConfigState> =_currentConnectionStatus.asStateFlow()
+        init {
+            checkConnectionStatus()
+        }
 
-    fun checkConnectionStatus(): NetworkConnection{
-     TODO()
+    fun checkConnectionStatus(): NetworkConnection? {
+        val currentInstance = _currentConnectionStatus.value.konnection
+        _currentConnectionStatus.update { currentState ->
+            currentState.copy(currentConnectionStatus = currentInstance.getCurrentNetworkConnection())
+        }
+     return currentConnectionState.value.currentConnectionStatus
     }
 
 }
