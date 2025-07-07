45667ef4652e3f4b9aa6e3ed9dac0c9dee093257
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-07 16:31:01 +0200

    Implement methods in SensorRepositoryImpl
    This commit implements the following methods in `SensorRepositoryImpl`:
    - `fetchHistoricSensorReadings`
    - `toggleLight`
    - `toggleFan`
    - `toggleExtractor`
    - `togglePump`
    - `ec`
    - `ecUp`
    - `ecDown`
    - `pH`
    - `pHUp`
    - `pHDown`
    
    Each of these methods now calls the corresponding method in the `SensorAPI`.

--- composeApp/src/commonMain/kotlin/org/smartroots/data/repository/SensorRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/repository/SensorRepositoryImpl.kt
@@ -8,6 +8,9 @@ import org.koin.dsl.module
 import org.smartroots.data.model.Sensor
 import org.smartroots.data.service.SensorAPI
 
+/**
+ *  @author Shravan Ramjathan
+ */
 class SensorRepositoryImpl(val baseURL: String, val sensorAPI: SensorAPI): SensorRepository {
 
     override suspend fun fetchSensorReading(): Sensor {
@@ -15,47 +18,47 @@ class SensorRepositoryImpl(val baseURL: String, val sensorAPI: SensorAPI): Senso
     }
 
     override suspend fun fetchHistoricSensorReadings(): List<Sensor> {
-        TODO("Not yet implemented")
+        return sensorAPI.getHistoricSensorReading()
     }
 
     override suspend fun toggleLight(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.toggleLight()
     }
 
     override suspend fun toggleFan(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.toggleFan()
     }
 
     override suspend fun toggleExtractor(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.toggleExtractor()
     }
 
     override suspend fun togglePump(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.togglePump()
     }
 
     override suspend fun ec(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.ec()
     }
 
     override suspend fun ecUp(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.ecUp()
     }
 
     override suspend fun ecDown(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.ecDown()
     }
 
     override suspend fun pH(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.pH()
     }
 
     override suspend fun pHUp(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.pHUp()
     }
 
     override suspend fun pHDown(): HttpResponse {
-        TODO("Not yet implemented")
+        return sensorAPI.pHDown()
     }
 }
 val sensorRepositoryModule = module{
