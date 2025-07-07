e5aa5746f344f5d5c5e9114f05a08e585c8da0f6
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-07 16:42:53 +0200

    Implement NetworkConfigRepositoryImpl
    This commit introduces the `NetworkConfigRepositoryImpl` class.
    
    This class provides methods to:
    - Check the current network connection status (`checkConnectionStatus`).
    - Retrieve detailed network connection information (`checkConnectionInfo`).
    
    It relies on a `NetworkConfig` dependency for these operations.

--- composeApp/src/commonMain/kotlin/org/smartroots/data/repository/NetworkConfigRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/data/repository/NetworkConfigRepositoryImpl.kt
@@ -1,2 +1,15 @@
 package org.smartroots.data.repository
 
+import dev.tmapps.konnection.NetworkConnection
+import org.smartroots.data.module.NetworkConfig
+
+class NetworkConfigRepositoryImpl(val networkConfig: NetworkConfig){
+
+    fun checkConnectionStatus(): NetworkConnection?{
+        return networkConfig.checkConnectionStatus()
+    }
+
+    suspend fun checkConnectionInfo():Map<NetworkConnection, String>{
+        return networkConfig.checkNetworkInfo()
+    }
+}
