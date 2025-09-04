290f469c2bbbbd8ff0ac6beb4c248f163cb0186d
ShravanRamjathan <shravanramjathan@gmail.com>
2025-09-05 01:04:40 +0200

    Update Kotlin JVM target and dependencies
    This commit updates the Kotlin JVM target to version 21 in `composeApp/build.gradle.kts`.
    
    It also introduces the `material-theme` dependency in `gradle/libs.versions.toml` and removes the `DropdownMenuWithDetails` composable function from `HomeScreen.kt`.

--- composeApp/src/commonMain/kotlin/org/smartroots/presentation/screens/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/org/smartroots/presentation/screens/HomeScreen.kt
@@ -2,7 +2,6 @@ package org.smartroots.presentation.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -16,19 +15,11 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import  androidx.compose.runtime.setValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -109,61 +100,7 @@ fun HomeScreen(viewModel: HomeViewModel = koinViewModel<HomeViewModel>()) {
         )
     }
 }
-@Composable
-fun DropdownMenuWithDetails() {
-    var expanded by remember { mutableStateOf(false) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
-    ) {
-        IconButton(onClick = { expanded = !expanded }) {
-            Icon(Icons.Default.MoreVert, contentDescription = "More options")
-        }
-        DropdownMenu (
-            expanded = expanded,
-            onDismissRequest = { expanded = false }
-        ) {
-            // First section
-            DropdownMenuItem(
-                text = { Text("Profile") },
-                leadingIcon = { Icon(Icons.Outlined.Person, contentDescription = null) },
-                onClick = { /* Do something... */ }
-            )
-            DropdownMenuItem(
-                text = { Text("Settings") },
-                leadingIcon = { Icon(Icons.Outlined.Settings, contentDescription = null) },
-                onClick = { /* Do something... */ }
-            )
-
-            HorizontalDivider()
-
-            // Second section
-            DropdownMenuItem(
-                text = { Text("Send Feedback") },
-                leadingIcon = { Icon(Icons.Outlined.Feedback, contentDescription = null) },
-                trailingIcon = { Icon(Icons.AutoMirrored.Outlined.Send, contentDescription = null) },
-                onClick = { /* Do something... */ }
-            )
-
-            HorizontalDivider()
-
-            // Third section
-            DropdownMenuItem(
-                text = { Text("About") },
-                leadingIcon = { Icon(Icons.Outlined.Info, contentDescription = null) },
-                onClick = { /* Do something... */ }
-            )
-            DropdownMenuItem(
-                text = { Text("Help") },
-                leadingIcon = { Icon(Icons.AutoMirrored.Outlined.Help, contentDescription = null) },
-                trailingIcon = { Icon(Icons.AutoMirrored.Outlined.OpenInNew, contentDescription = null) },
-                onClick = { /* Do something... */ }
-            )
-        }
-    }
-}
 @Composable
 fun SensorCard(title: String, iconRes: DrawableResource, value: String) {
     Card(
